ea14956bdd436170d557d28fde43bba4a62bf55d
ArthurMaciel95 <arthurnmrocha@gmail.com>
2022-01-22 18:54:09 -0300

    :art: adicionando botao para esconde senha

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -17,48 +17,62 @@ export const Container = styled.form`
     }
 `
 export const Group = styled.div`
-    display: flex;
+position: relative;
+display: flex;
+width: 100%;
+flex-direction: column;
+gap: 1px;
+margin:0.30rem;
+margin-bottom: 0.6rem;
+justify-content: space-between;
+height: 100%;
+label {
+    color: #18191F;
+    font-size: 0.85rem;
+}
+input {
     width: 100%;
-    flex-direction: column;
-    gap: 1px;
-    margin:0.30rem;
-    margin-bottom: 0.6rem;
-    justify-content: space-between;
-    height: 100%;
-    label {
-        color: #18191F;
-        font-size: 0.85rem;
+    height: 46px;
+    border:none;
+    padding: 1rem;
+    background-color: #fff;
+    font-size: 1.1rem;
+    margin-top:10px;
+    box-shadow:inset 0px 1px 4px var(--text-color-gray);
+    color:var(--text-color-header);
+    &:focus {
+      outline: none;
     }
-    input {
-        width: 100%;
-        height: 46px;
-        border:none;
-        padding: 1rem;
-        background-color: #fff;
-        font-size: 1.1rem;
-        margin-top:10px;
-        box-shadow:inset 0px 1px 4px var(--text-color-gray);
-        color:var(--text-color-header);
-        &:focus {
-          outline: none;
-        }
-        &.round {
-            border-radius: 25px;
-        }
+    &.round {
+        border-radius: 25px;
     }
-    textarea{
-        outline:none;
-        padding: 1rem;
-        resize:none;
-        min-height:200px;
-        box-shadow:inset 0px 2px 4px var(--text-color-gray);
-        border:none;
-        &.round {
-            border-radius:25px;
-        }
+}
+img{
+    height: 30px;
+    width: 30px;
+    filter: invert(68%) sepia(90%) saturate(11%) hue-rotate(314deg) brightness(90%) contrast(87%);
+    position: absolute;
+    right:20px;
+    top:17px;
+    cursor: pointer;
+    
+}
+img::hover{
+    filter: invert(55%) sepia(22%) saturate(4374%) hue-rotate(323deg) brightness(112%) contrast(111%);
+}
+textarea{
+    outline:none;
+    padding: 1rem;
+    resize:none;
+    min-height:200px;
+    box-shadow:inset 0px 2px 4px var(--text-color-gray);
+    border:none;
+    &.round {
+        border-radius:25px;
+    }
 
-        
-    }
+    
+}
 `
 export const GroupContainer = styled.div`
     display: flex;
