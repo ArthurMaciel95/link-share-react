cc276634086a02ff161b931ffae635696609cba8
Danrlei <contato@dscoding.com.br>
2022-04-07 00:39:27 -0300

    :recycle: Update baseURL

--- src/environment/index.js
+++ src/environment/index.js
@@ -1,5 +1,5 @@
 export default {
-    // baseURL: "http://localhost:8080/",                       //Local
-     baseURL: "https://sharelink-development.herokuapp.com/", //Desenvolvimento
+    // baseURL: "http://localhost:8080/",                           //Local
+     baseURL: "https://sharelink-development.herokuapp.com/",      //Desenvolvimento
     // baseURL: "https://api-sharelink.herokuapp.com/",            //Produção
 };
--- src/services/api/file.js
+++ src/services/api/file.js
@@ -3,7 +3,7 @@ import environment from "environment";
 import { getToken } from "utils/jwt";
 
 export function FileBase() {
-    const instance = axios.create({ baseURL: environment.baseUrl });
+    const instance = axios.create({ baseURL: environment.baseURL });
     instance.interceptors.request.use((config) => {
         const token = getToken();
         if (token) config.headers[`x-access-token`] = token;
