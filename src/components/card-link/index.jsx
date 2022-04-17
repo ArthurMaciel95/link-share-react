31b8d7a6d6db6fa62c8ba7cf86a1cb19abdb1a53
ArthurMaciel95 <arthurnmrocha@gmail.com>
2022-04-17 03:29:39 -0300

    :art: adicionando sidebar bottom em cards

--- src/components/card-link/index.jsx
+++ src/components/card-link/index.jsx
@@ -3,18 +3,78 @@ import { Card } from "./styles.js";
 import threeDots from "../../assets/svg/three-dots.svg";
 import ModalCardSettings from "components/modal-card-settings";
 import { toast } from "react-toastify";
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { useNavigate } from "react-router-dom";
+import { LinksUrls } from "services/api/link";
+
 const CardLink = ({
     name,
     image,
     link,
-    handlerRemoveLink,
     createAt,
     id,
     visitor,
     Key,
 }) => {
-    const [openModalSettings, setOpenModalSettings] = useState(false);
-    const handlerClipBoard = () => {};
+    const [open, setOpen] = useState(false);
+
+    let anchor = 'bottom'
+    const navigate = new useNavigate();
+    const linkService = new LinksUrls();
+
+    const handlerRemoveLink = async (e) => {
+        try {
+            const result = await linkService.unregister(id);
+            toast.success("link deleted successfully!");
+        } catch (err) {
+            toast.error("error trying to delete link");
+
+            console.log(err);
+        }
+    };
+
+    const handlerClipBoard = () => {
+        navigator.clipboard.writeText(link);
+        toast.success("link successfully copied!");
+    };
+    const toggleDrawer = (open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        setOpen(open)
+    };
+
+    const list = () => (
+        <Box
+            sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+            role="presentation"
+            onClick={toggleDrawer(false)}
+            onKeyDown={toggleDrawer(false)}
+        >
+            <List>
+                {[{ text: 'Delete URL', icon: <DeleteIcon />, action: handlerRemoveLink }, { text: 'Copy Link', icon: <ContentCopyIcon />, action: handlerClipBoard }].map(({ text, icon, action }, index) => (
+                    <ListItem button key={text} onClick={action}>
+                        <ListItemIcon>
+                            {icon}
+                        </ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+
+
+                ))}
+            </List>
+        </Box>
+    );
 
     return (
         <Card key={Key}>
@@ -36,16 +96,28 @@ const CardLink = ({
                     src={threeDots}
                     alt="icon settings"
                     className="btn-settings"
-                    onClick={() => setOpenModalSettings(!openModalSettings)}
+                    onClick={toggleDrawer(true)}
                 />
-                {openModalSettings && (
+                {/*     {openModalSettings && (
                     <ModalCardSettings
                         id={id}
                         link={link}
                         visitor={visitor}
                         handlerRemoveLink={handlerRemoveLink}
                     />
-                )}
+                )} */}
+
+
+
+                < Drawer
+                    anchor={anchor}
+                    open={open}
+                    onClose={toggleDrawer(false)}
+                >
+                    {list()}
+                </Drawer>
+
+
             </div>
         </Card>
     );
