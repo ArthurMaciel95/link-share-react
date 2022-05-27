7c8602bf08cf8e6393bb3fe7e02571481064e22e
ArthurMaciel95 <arthurnmrocha@gmail.com>
2022-05-26 23:18:11 -0300

    :bug: rota de adicionar link funcionando

--- src/services/link.js
+++ src/services/link.js
@@ -3,7 +3,7 @@ import { API } from "./axios";
 export class LinksServices {
     static requests = API([["Content-Type", "application/json"]]);
     static async create(form) {
-        return this.requests.post('/link/create', { form });
+        return this.requests.post('/link/create', form);
     }
     static async delete(linkId) {
         return this.requests.delete(`/link/${linkId}/*`);
