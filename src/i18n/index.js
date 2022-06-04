6c82c70c0361811e180491414c0b9b7f6535ea39
ArthurMaciel95 <arthurnmrocha@gmail.com>
2022-06-04 16:17:41 -0300

    :memo: adicionando arquivo para contribuição para o projeto.

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -3,24 +3,21 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import en from './translations/en.json';
 import pt from './translations/pt.json'
-// the translations
-// (tip move them in a JSON file and import them,
-// or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
+
+// import {{idioma}} from './translations/{{idioma}}.json'
 
 
 i18n
-    .use(initReactI18next) // passes i18n down to react-i18next
+    .use(initReactI18next)
     .init({
         fallbackLng: 'pt',
-        // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
-        // if you're using a language detector, do not define the lng option
-
         interpolation: {
-            escapeValue: false // react already safes from xss
+            escapeValue: false
         },
         resources: {
             en,
-            pt
+            pt,
+            // adicionar variável com idioma importada.
         }
     });
 
